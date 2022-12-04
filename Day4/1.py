5ad064fe0de5f62e277bc007be05f29833d97d1a
sebastian <se4872ma-s@lu.student.se>
2022-12-04 18:17:33 +0100

    Working day4

---
+++ Day4/1.py
@@ -0,0 +1,18 @@
+
+path =  r'./input1.in'
+input_file = open(path, 'r')
+my_list = input_file.read().split('\n')
+
+count = 0
+
+for x in my_list:
+    splitX = x.split(',')
+    string1 = splitX[0].split('-')
+    string2 = splitX[1].split('-')
+    set1 = set(range(int(string1[0]), int(string1[1]) +1))
+    set2 = set(range(int(string2[0]), int(string2[1]) +1))
+    if set1.issubset(set2):
+        count += 1
+    elif set2.issubset(set1):
+        count += 1
+print(count)
